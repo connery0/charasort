a84d2f7d6d60635c7c0cde596072bf1030047b07
connery0 <onmymarkburn4321burn@gmail.com>
2024-07-21 15:07:34 +0200

    temp player characters

--- src/js/data/2024-07-21.js
+++ src/js/data/2024-07-21.js
@@ -4,68 +4,56 @@ dataSet[dataSetVersion] = {};
 //Other filters: player, campaign, class, artist? npc
 
 dataSet[dataSetVersion].options = [
-  // {name: "Filter nuns"}
 
-  // {
+  {
+    name:"Filter by Collection",
+    key: "Collection",
+    tooltip: "Add characters to the pool based on a theme",
+    checked:true,
 
-    
-  //   name: "Filter by Theme",
-  //   key: "series",
-  //   tooltip: "Check this to restrict to certain series.",
-  //   checked: false,
-  //   sub: [
-  //     { name: "Books and CDs", key: "book" },
-  //     { name: "The Highly Responsive to Prayers", tooltip: "01 - Reiiden", key: "HRtP" },
-  //     { name: "The Story of Eastern Wonderland", tooltip: "02 - Fuumaroku", key: "SoEW" },
-  //     { name: "Phantasmagoria of Dim.Dream", tooltip: "03 - Yumejikuu", key: "PoDD" },
-  //     { name: "Lotus Land Story", tooltip: "04 - Gensoukyou", key: "LLS" },
-  //     { name: "Mystic Square", tooltip: "05 - Kaikidan", key: "MS" },
-  //     { name: "Embodiment of Scarlet Devil", tooltip: "06 - Koumakan", key: "EoSD" },
-  //     { name: "Perfect Cherry Blossom", tooltip: "07 - Youyoumu", key: "PCB" },
-  //     { name: "Immaterial and Missing Power", tooltip: "07.5 - Suimusou", key: "IaMP" },
-  //     { name: "Imperishable Night", tooltip: "08 - Eiyashou", key: "IN" },
-  //     { name: "Phantasmagoria of Flower View", tooltip: "09 - Kaeidzuka", key: "PoFV" },
-  //     { name: "Shoot the Bullet", tooltip: "09.5 - Bunkachou", key: "StB" },
-  //     { name: "Mountain of Faith", tooltip: "10 - Fuujinroku", key: "MoF" },
-  //     { name: "Scarlet Weather Rhapsody", tooltip: "10.5 - Hisouten", key: "SWR" },
-  //     { name: "Subterranean Animism", tooltip: "11 - Chireiden", key: "SA" },
-  //     { name: "Undefined Fantastic Object", tooltip: "12 - Seirensen", key: "UFO" },
-  //     { name: "Touhou Hisoutensoku", tooltip: "12.3 - Hisoutensoku", key: "Soku" },
-  //     { name: "Double Spoiler", tooltip: "12.5 - Bunkachou", key: "DS" },
-  //     { name: "Great Fairy Wars", tooltip: "12.8 - Daisensou", key: "GFW" },
-  //     { name: "Ten Desires", tooltip: "13 - Shinreibyou", key: "TD" },
-  //     { name: "Hopeless Masquerade", tooltip: "13.5 - Shinkirou", key: "HM" },
-  //     { name: "Double Dealing Character", tooltip: "14 - Kishinjou", key: "DDC" },
-  //     { name: "Impossible Spell Card", tooltip: "14.3 - Amanojaku", key: "ISC" },
-  //     { name: "Urban Legend in Limbo", tooltip: "14.5 - Shinpiroku", key: "ULiL" },
-  //     { name: "Legacy of Lunatic Kingdom", tooltip: "15 - Kanjuden", key: "LoLK" }
-  //   ]
-  // },
-  // {
-  //   name: "Filter by Stage Enemy Appearances",
-  //   key: "stage",
-  //   tooltip: "Check this to restrict to characters that appear in certain stages as enemies.",
-  //   checked: false,
-  //   sub: [
-  //     { name: "Stage 1", key: "st1" },
-  //     { name: "Stage 2", key: "st2" },
-  //     { name: "Stage 3", key: "st3" },
-  //     { name: "Stage 4", key: "st4" },
-  //     { name: "Stage 5/Penultimate", tooltip: "Stage 4 in 5-stage games, and Stage 8 in 9-stage games.", key: "st5" },
-  //     { name: "Stage 6/Final", key: "st6" },
-  //     { name: "Stage EX/Phantasm", key: "ex" }
-  //   ]
-  // },
-  // {
-  //   name: "Remove PC-98 Duplicates",
-  //   key: "pc98",
-  //   tooltip: "Check this to remove PC-98 characters with a Windows counterpart."
-  // },
-  // {
-  //   name: "Remove Non-Girls",
-  //   key: "notgirl",
-  //   tooltip: "Check this to remove all non-female characters."
-  // }
+    sub:
+      [
+        {
+          name:"ONLY Nuns from corrupt the nun",
+          key:"CTNun",
+          tooltip: "Titualar nuns",
+          checked:true          
+        }
+      ]
+  },  
+  {
+    name:"Filter by Campaign",
+    key: "Campaign",
+    tooltip: 'Check this if you want to filter out certain campaigns.',
+    checked:true,
+
+    sub:
+      [
+        {
+          name:"Corrupt the Nun",
+          key:"CTNunCampaign"   
+        }
+      ]
+  },
+
+  {
+    name: "Remove npcs",
+    key: "NPC",       //Anything that contains this key (set to true) will be REMOVED from the pool
+    tooltip: "Hide non player characters", // optional
+    checked: false, // optional
+  },
+  {
+    name: "Remove PCs",
+    key: "PC",       //Anything that contains this key (set to true) will be REMOVED from the pool
+    tooltip: "Hide player characters", // optional
+    checked: false, // optional
+  },
+  {
+    name: "Remove variants",
+    key: "Variant",       //Anything that contains this key (set to true) will be REMOVED from the pool
+    tooltip: "Hide options marked as variant", // optional
+    checked: false, // optional
+  }
 ];
 
 dataSet[dataSetVersion].characterData = [
@@ -73,56 +61,103 @@ dataSet[dataSetVersion].characterData = [
     name: "Helsee",
     img: "Md5FAW6.png",
     opts: {
-      collection: 'Nun'
+      Campaign: ["CTNunCampaign"],
+      Collection: ['CTNun'],
+      NPC:true
     }
   },
   {
     name: "Kirisame Marisa",
     img: "WGxIAP8.jpeg",
     opts: {
-      collection: 'Nun'
+      Campaign: ["CTNunCampaign"],
+      Collection: ['CTNun'],
+      NPC:true
     }
   },{
     name:"Hina",
     img:"r30wjeu.jpeg",
     opts:{
-      collection: 'Nun'
+      Campaign: ["CorruptNun"],
+      Collection: ['CTNun'],
+      NPC:true
     }
   },{
     name:"Aria",
     img:"PMixEdg.png",
     opts:{
-      collection: 'Nun'
+      Campaign: ["CTNunCampaign"],
+      Collection: ['CTNun'],
+      NPC:true
     }
   },{
     name:"Kashmir",
     img:"zeydGZ0.jpeg",
     opts:{
-      collection: 'Nun'
+      Campaign: ["CTNunCampaign"],
+      Collection: ['CTNun'],
+      NPC:true
     }
   },{
     name:"Halphas",
     img:"GUZHwn6.jpeg",
     opts:{
-      collection: 'Nun'
+      Campaign: ["CTNunCampaign"],
+      Collection: ['CTNun'],
+      NPC:true
     }
   },{
     name:"Malphas (Hot)",
     img:"G0VrAYf.jpeg",
     opts:{
-      collection: 'Nun'
+      Campaign: ["CTNunCampaign"],
+      Collection: ['CTNun'],
+      NPC:true
     }
   },{
     name:"Nuolankumas",
     img:"kE463xR.jpeg",
     opts:{
-      collection: 'Nun'
+      Campaign: ["CTNunCampaign"],
+      Collection: ['CTNun'],
+      NPC:true
     }
   },{
     name:"Malphas (Kalm)",
     img:"sGeJCR2.png",
     opts:{
-      collection: 'Nun'
+      Campaign: ["CTNunCampaign"],
+      Collection: ['CTNun'],
+      NPC:true,
+      Variant:true
+    }
+  },{
+    name:"Beliel",
+    img:"FeGb7xy.png",
+    opts:{
+      Campaign: ["CTNunCampaign"],
+      PC:true
+    }
+  },{
+    name:"Yooni",
+    img:"XzHXrV3.png",
+    opts:{
+      Campaign: ["CTNunCampaign"],
+      PC:true
+    }
+  },{
+    name:"celene",
+    img:"83tf4lX.png",
+    opts:{
+      Campaign: ["CTNunCampaign"],
+      PC:true
+    }
+  },{
+    name:"Ikki",
+    img:"LU1i7Ni.png",
+    opts:{
+      Campaign: ["CTNunCampaign"],
+      PC:true
     }
   }
 ];
